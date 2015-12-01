43bed5b8f9abd899283b98fe342405682729da1a
Tyler Waltze <tylerwaltze@gmail.com>
2015-11-30 22:59:32 -0500

    Incorporate boilerplate code/build system

--- src/js/message.js
+++ src/js/message.js
@@ -3,14 +3,14 @@
 var Message = function(key, message, hashes) {
     this.message = message;
     this.key = sjcl.codec.utf8String.toBits(key);
-    this.hashes = [];
+    this.hashes = hashes || [];
 
     this.randomStr = function(length) {
         var ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
-        var str = "";
-        for (var i=0; i < length; ++i) {
+        var str = '';
+        for (var i = 0; i < length; ++i) {
             var rand = Math.floor(Math.random() * ALPHABET.length);
-            str += ALPHABET.substring(rand, rand+1);
+            str += ALPHABET.substring(rand, rand + 1);
         }
         return str;
     };
@@ -19,7 +19,7 @@ var Message = function(key, message, hashes) {
         var randomCharacter = this.randomStr(1);
         var randomKey = sjcl.codec.utf8String.toBits(this.randomStr(10));
         var noiseMac = new sjcl.misc.hmac(randomKey);
-    	var noiseHash = noiseMac.encrypt(randomCharacter);
+        var noiseHash = noiseMac.encrypt(randomCharacter);
 
         return {
             'character': randomCharacter,
@@ -28,32 +28,35 @@ var Message = function(key, message, hashes) {
     };
 
     this.shuffle = function(array) {
-      var currentIndex = array.length, temporaryValue, randomIndex ;
+        var currentIndex = array.length;
+        var temporaryValue;
+        var randomIndex;
 
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
 
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
 
-      return array;
-    }
-}
+        return array;
+    };
+};
 
 Message.prototype.deconstruct = function() {
     var _self = this;
+
     // Break message into characters
     this.message.split('').forEach(function(character, index) {
         // Generate valid hmac for this character
         var mac = new sjcl.misc.hmac(_self.key);
-    	var hash = mac.encrypt(character);
+        var hash = mac.encrypt(character);
         _self.hashes.push({
             'character': character,
             'hash': hash.join(''),
@@ -67,14 +70,14 @@ Message.prototype.deconstruct = function() {
     });
 
     return this.shuffle(this.hashes);
-}
+};
 
 Message.prototype.reconstruct = function() {
     var _self = this;
     var rebuilt = [];
     this.hashes.forEach(function(h) {
         var mac = new sjcl.misc.hmac(_self.key);
-    	var hash = mac.encrypt(h.character).join('');
+        var hash = mac.encrypt(h.character).join('');
 
         if (hash === h.hash) {
             rebuilt[h.index] = h.character;
@@ -82,4 +85,4 @@ Message.prototype.reconstruct = function() {
     });
 
     return rebuilt.join('');
-}
+};
